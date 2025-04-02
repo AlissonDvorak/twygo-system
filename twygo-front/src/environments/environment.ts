5f3db64b824d4191c46ad947103a82e516f1ec0b
Alisson <alisson.dvorak@wises.com.br>
2025-04-02 18:39:49 -0300

    add: localhost

--- twygo-front/src/environments/environment.ts
+++ twygo-front/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://82.29.58.93:8999/api/'
+    apiUrl: 'http://localhost:8999/api/'
     // apiUrl: 'http://localhost:8000/api/' para development
 
   };
