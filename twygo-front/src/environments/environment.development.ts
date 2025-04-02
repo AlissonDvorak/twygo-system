7d999ef4bb5b2c467274513536119d1bb4441bb1
Alisson <alisson.dvorak@wises.com.br>
2025-04-02 16:19:37 -0300

    fix: to deploy

--- twygo-front/src/environments/environment.development.ts
+++ twygo-front/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://127.0.0.1:8999/api/'
+  apiUrl: 'http://localhost:8999/api/'
   // apiUrl: 'http://localhost:8000/api/' para development
 };
--- twygo-front/src/environments/environment.ts
+++ twygo-front/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://127.0.0.1:8999/api/'
+    apiUrl: 'http://localhost:8999/api/'
     // apiUrl: 'http://localhost:8000/api/' para development
 
   };
